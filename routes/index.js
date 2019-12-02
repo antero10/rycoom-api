22fb5461aad4c1c8cc7a94445356fd16dfdad8f5
Gustavo Sanchez <antero105@gmail.com>
2019-12-02 13:47:59 -0500

    cratinga pi

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET home page. */
+router.post('/email', async function(req, res, next) {
+  try {
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: req.body.to,
+        from: 'test@test.com',
+        subject: req.body.subject,
+        html: `<p>Company: ${req.body.company}</p>`,
+        attachments: req.body.attachments,
+    };
+    await sgMail.send(msg);
+    res.status(200).send();
+  } catch(e) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
